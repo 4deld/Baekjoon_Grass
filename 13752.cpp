b3131bca3d798051577e31a09a17ae6bd35c765c
rlapo213 <rlapo@naver.com>
2022-09-25 03:51:50 +0900

    update

---
+++ 13752.cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,k;
+	cin >> n;
+	while (n--) {
+		cin >> k;
+		for (int i = 0; i < k; i++)
+			cout << '=';
+		cout << '\n';
+	}
+
+	return 0;
+}

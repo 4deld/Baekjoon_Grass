57bff8e4b303abb6844505e4e0b2fba0df711874
rlapo213 <rlapo@naver.com>
2022-09-28 01:56:10 +0900

    ,,

---
+++ 15151.cpp
@@ -0,0 +1,14 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int a, b,cnt;
+int main() {
+	cin >> a >> b;
+	while (1) {
+		b -= (a << cnt);
+		if (b < 0)
+			break;
+		cnt++;
+	}
+	cout << cnt;
+	return 0;
+}

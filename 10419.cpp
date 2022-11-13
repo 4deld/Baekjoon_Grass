5075c3e8b0a6dde099318f64573c3c488cfcc54e
rlapo213 <rlapo@naver.com>
2022-11-14 02:15:35 +0900

    Create 10419.cpp

---
+++ 10419.cpp
@@ -0,0 +1,14 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int t, d;
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> d;
+		int i = sqrt(d);
+		for (; ; i--) {
+			if (i * i + i <= d)break;
+		}
+		cout<< i << '\n';
+	}
+}
